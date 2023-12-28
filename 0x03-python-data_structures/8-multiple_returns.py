c8cc1463b9ccb98912291b5942b3e3942b72fd88
sheissadi729 <sadicisse729@gmail.com>
2023-12-28 23:16:22 +0000

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    my_tuple = ()
-    if len(sentence) == 0:
-        my_tuple = 0, "None"
+    """ returns a tuple with the length of a string and its first character"""
+    if sentence == "":
+        my_tuple = len(sentence), None
     else:
         my_tuple = len(sentence), sentence[0]
     return my_tuple
