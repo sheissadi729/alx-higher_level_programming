9c258c9c5f7382118395369afde54d6fac301ec0
sheissadi729 <sadicisse729@gmail.com>
2023-12-28 15:35:01 +0000

    handle case list == None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,8 @@
 
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list, in reverse order"""
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list == None:
+        pass
+    else:
+        for i in range(len(my_list) - 1, -1, -1):
+             print("{:d}".format(my_list[i]))
