e47bc63652fe1041cad992699be14cd5af24588f
sheissadi729 <sadicisse729@gmail.com>
2023-12-28 23:27:50 +0000

    handle empy list case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 
 def max_integer(my_list=[]):
     """ finds the biggest integer of a list"""
+    if my_list == []:
+        return None
     maxi = 0
     for num in my_list:
         if num > maxi:
