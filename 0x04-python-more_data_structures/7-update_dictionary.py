7307862e79ab38806815cefc77f1905dee59dcb0
sheissadi729 <sadicisse729@gmail.com>
2023-12-30 21:15:55 +0000

    tasks 6 and 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 def update_dictionary(a_dictionary, key, value):
-    """
-    A function that replaces or adds
-    key/value in a dictionary.
-    """
     a_dictionary[key] = value
     return a_dictionary
