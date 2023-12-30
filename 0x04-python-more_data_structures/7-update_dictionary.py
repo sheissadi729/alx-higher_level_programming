50a4d9f98c12d0bc9446881a6588a676df675bc3
sheissadi729 <sadicisse729@gmail.com>
2023-12-30 21:17:09 +0000

    fix pycodestyle

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
