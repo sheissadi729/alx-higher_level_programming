b32e4aa73448a27aba73c9c100ec0e89dfefbcd8
sheissadi729 <sadicisse729@gmail.com>
2023-12-30 22:14:29 +0000

    fix pycodestyle

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 def simple_delete(a_dictionary, key=""):
     if key in list(a_dictionary):
