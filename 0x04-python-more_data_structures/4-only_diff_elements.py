33bf7ab74a55fc104df0d785b2f2b6ee19aea08d
sheissadi729 <sadicisse729@gmail.com>
2023-12-30 20:17:29 +0000

    remove test

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,8 +4,3 @@ def only_diff_elements(set_1, set_2):
     """returns a set of all elements present in only one set"""
     new_set = set_1 ^ set_2
     return new_set
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
