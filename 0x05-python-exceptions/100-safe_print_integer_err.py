d4211801fd6d9052cf1352a9e8e23722a2f11a1f
sheissadi729 <sadicisse729@gmail.com>
2024-01-01 18:15:34 +0000

    fix bug task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+import sys
 
 def safe_print_integer_err(value):
     '''prints an integer'''
     try:
         print("{:d}".format(value))
     except ValueError as ve:
-        print("Exception:", ve)
+        print("Exception:", ve, file=sys.stderr)
         return False
     except TypeError as te:
-        print("Exception:", te)
+        print("Exception:", te, file=sys.stderr)
         return False
     return True
