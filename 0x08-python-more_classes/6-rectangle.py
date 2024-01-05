040dd922d7827dd191c1b3435d3805e3696cf302
sheissadi729 <sadicisse729@gmail.com>
2024-01-05 03:01:41 +0000

    fix pycodestyle task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,11 @@
 class Rectangle:
     """represent a rectangle"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initialize a new rectangle"""
         self.width = width
-        self.height = height 
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
